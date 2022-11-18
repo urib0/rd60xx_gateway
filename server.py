a28973f4a26b2bcd58d0aa49692dd336ca2f7298
uribo <ur1b0.dev@gmail.com>
2022-11-18 18:57:25 +0900

    rd60xxとの疎通確認を追加

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 from ast import arg
 import sys
 
+from rd6006 import RD6006
+
 # 引数チェック
 args = sys.argv
 if len(args) == 1:
@@ -12,5 +14,10 @@ elif len(args) > 2:
 else:
     device_path = args[1]
 
-print(device_path)
+# rd60xxと疎通確認
+device = RD6006(device_path)
+print(f"model:{device.type}")
+print(f"fw:{device.fw}")
+device.enable = False
+print(f"output:{True if device.enable == 1 else False }")
 
